89983dd351788e80f97c4fc3abada032fd82988c
nksprzak <nksprzak@gmail.com>
2015-09-24 16:11:27 -0500

    Added Vote and Canidate Classes close #6, close #7

--- Voting.h
+++ Voting.h
@@ -5,8 +5,27 @@
 #include <iostream> // istream, ostream
 #include <string>   // string
 #include <utility>  // pair
+#include <vector>
 
 using namespace std;
+
+class Vote
+{
+	public:
+		vector<int> prefs;
+		int index = 0;
+};
+
+class Canidate
+{
+	public:
+		string name;
+		int vote_total = 0;
+		int can_num;
+		vector<Vote> votes;
+		bool is_loser = false;
+};
+
 
 
 void voting_solve(istream& r, ostream& w);
